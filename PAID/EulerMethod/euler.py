a6deeff22c672b742ff5c8cc5091d3c77cd72da4
DavAug <david.augustin@gmx.net>
2019-11-04 16:12:21 +0000

    Euler Method

--- PAID/EulerMethod/euler.py
+++ PAID/EulerMethod/euler.py
@@ -1,2 +1,16 @@
+import numpy as np
+
 class euler(object):
-    def __init__(self, )
+    def __init__(self, func):
+        self.func = func
+
+    def integrate(self, dt, t_0, t_final, y_0):
+        times = np.arange(t_0, t_final, dt)
+        y_solution = np.empty(len(times))
+        for step, time_steps in enumerate(times):
+            if step == 0:
+                y_solution[step] = y_0
+            else:
+                y_solution[step] = y_solution[step-1] + dt * self.func(time_steps-dt, y_solution[step-1])
+
+        return y_solution
--- tests/EulerMethod/test_euler.py
+++ tests/EulerMethod/test_euler.py
@@ -5,6 +5,32 @@ import numpy as np
 
 from PAID.EulerMethod import euler
 
+
+def test_euler():
+    '''
+    Testing the numerical solution of the implemented Euler method.
+    '''
+    ### model parameters
+    Lambda = 1.0
+    C = 100.0
+
+    # integration paramters
+    dt = 0.01 # time steps
+    t_0 = 0.0 # initial time
+    t_final = 10.0 # final time
+    y_0 = 1.0 # initial condition
+
+    times = np.arange(start=t_0, stop=t_final, step=dt)
+    analytic = logistic_growth(times, C=C, Lambda=Lambda)
+    # logistic growth ODE
+    func = lambda t, N: Lambda * N * (1 - N / C)
+    logistic_model = euler.euler(func)
+    numeric = logistic_model.integrate(dt=dt, t_0=t_0, t_final=t_final, y_0=y_0)
+
+    tolerance = 1
+    assert np.all((analytic - numeric) ** 2 < tolerance ** 2)
+
+
 def logistic_growth(times, t_0=0.0, N_0=1.0, C=100.0, Lambda=1.0):
     '''
     Example ODE for testing that we know the analytical solution of.
@@ -22,14 +48,3 @@ def logistic_growth(times, t_0=0.0, N_0=1.0, C=100.0, Lambda=1.0):
     times += t_0 # applying set-off
     result = C / (1 + (C / N_0 - 1) * np.exp(-Lambda * times))
     return result
-
-def test_euler():
-    '''
-    Testing the numerical solution of the implemented Euler method.
-    '''
-    times = np.linspace(start=0, stop=10, num=100)
-    analytic = logistic_growth(times)
-    numeric = euler()
-
-    tolerance = 1e-05
-    assert (analytic - numeric) ** 2 < tolerance ** 2
