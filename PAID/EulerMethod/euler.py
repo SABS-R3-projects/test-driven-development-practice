72bd28ae466a806b81abfe2532548c4c3f1882be
DavAug <david.augustin@gmx.net>
2019-11-05 15:06:22 +0000

    docstring

--- PAID/EulerMethod/euler.py
+++ PAID/EulerMethod/euler.py
@@ -4,13 +4,34 @@ class euler(object):
     def __init__(self, func):
         self.func = func
 
-    def integrate(self, dt, t_0, t_final, y_0):
-        times = np.arange(t_0, t_final, dt)
-        y_solution = np.empty(len(times))
-        for step, time_steps in enumerate(times):
-            if step == 0:
-                y_solution[step] = y_0
-            else:
-                y_solution[step] = y_solution[step-1] + dt * self.func(time_steps-dt, y_solution[step-1])
+    def integrate(self, h, t_0, t_final, y_0):
+        """Method to find the numerical solution of the ODE in the interval [t_0, t_final].
 
-        return y_solution
+        Arguments:
+            h {float} -- step size of integrator.
+            t_0 {float} -- initial time of integration.
+            t_final {float} -- final time of integration.
+            y_0 {float} -- initial value of state variable.
+        
+        Returns:
+            y_solution {np.array} -- numerical solution to ODE.
+        """
+
+        # times = np.arange(t_0, t_final, h)
+        # y_solution = np.empty(len(times))
+        # for step, time_steps in enumerate(times):
+        #     if step == 0:
+        #         y_solution[step] = y_0
+        #     else:
+        #         y_solution[step] = y_solution[step-1] + h * self.func(time_steps-h, y_solution[step-1])
+
+        # return y_solution
+
+        times = [t_0]
+        y_solution = [y_0]
+        t_n = t_0
+        while t_n < t_final:
+            # checking whether step size provides desired accuracy.
+            epsilon = 
+            while epsilon > error:
+
