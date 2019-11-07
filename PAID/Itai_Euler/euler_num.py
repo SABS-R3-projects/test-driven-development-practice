a971cdfab97f6802ab15ba99cbdc0e877bafdf46
sabs-r3 <itaimuzh@gmail.com>
2019-11-07 15:14:12 +0000

    Embedded plotting function inside of the ODENumerical class

--- PAID/Itai_Euler/euler_num.py
+++ PAID/Itai_Euler/euler_num.py
@@ -22,18 +22,19 @@ class ODENumerical:
         return euler_array
 
 
-def plot_numerical_noise():
-    a = ODENumerical()
-    numerical_sol = np.asanyarray(a.euler_solution())
-    mu, sigma = 0, 0.5
-    # creating a noise with the same dimension as the dataset
-    noise = np.random.normal(mu, sigma, [np.shape(numerical_sol)[0], ])
-    numerical_noisy = numerical_sol + noise
-    index_array = np.arange(1,np.shape(numerical_sol)[0]+1)
-    plt.scatter(index_array, numerical_noisy)
-    plt.plot(numerical_sol, color = "black")
-    #plt.plot(numerical_noisy, "b.")
-    plt.show()
+    def plot_numerical_noise(self):
+        #a = ODENumerical()
+        numerical_sol = np.asanyarray(self.euler_solution())
+        mu, sigma = 0, 0.5
+        # creating a noise with the same dimension as the dataset
+        noise = np.random.normal(mu, sigma, [np.shape(numerical_sol)[0], ])
+        numerical_noisy = numerical_sol + noise
+        index_array = np.arange(1,np.shape(numerical_sol)[0]+1)
+        plt.scatter(index_array, numerical_noisy)
+        plt.plot(numerical_sol, color = "black")
+        #plt.plot(numerical_noisy, "b.")
+        plt.show()
 
-plot_numerical_noise()
+a = ODENumerical()
+a.plot_numerical_noise()
 
