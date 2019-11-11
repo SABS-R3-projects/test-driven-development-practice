ec7b5b599824c2e7a3728bb08cd2056af1b003bf
sabs-r3 <itaimuzh@gmail.com>
2019-11-11 10:02:00 +0000

    CMA-ES now functional and with graphs

--- PAID/Itai_Euler/euler_num.py
+++ PAID/Itai_Euler/euler_num.py
@@ -3,7 +3,7 @@ import numpy as np
 import cma
 class ODENumerical:
 
-    def __init__(self, lambda_val = 0.095, n_init = 1, c = 10):
+    def __init__(self, lambda_val = 0.095, n_init = 1, c = 100):
         self.lambda_val = lambda_val
         self.n_init = n_init
         self.c = c
@@ -18,7 +18,7 @@ class ODENumerical:
     def euler_solution(self):
         euler_array = [self.n_init]
         n = self.n_init
-        while len(euler_array) <= 150:
+        while len(euler_array) <= 250:
             n += self.dt*self.differential_func(n)
             euler_array.append(n)
         euler_array = np.asanyarray(euler_array)
@@ -27,7 +27,7 @@ class ODENumerical:
 
     def euler_with_noise(self):
         numerical_sol = self.euler_solution()
-        mu, sigma = 0, 0.5
+        mu, sigma = 0, 5
         # creating a noise with the same dimension as the dataset
         noise = np.random.normal(mu, sigma, [np.shape(numerical_sol)[0], ])
         numerical_noisy = numerical_sol + noise
@@ -39,7 +39,7 @@ class ODENumerical:
         euler_with_noise = self.euler_with_noise()
         x_values = np.arange(0,len(euler_array),1)
         plt.scatter(x_values,euler_with_noise, color = "yellow", edgecolors="black")
-        plt.scatter(x_values,euler_array, color = "red")
+        plt.plot(euler_array, color = "red")
         plt.ylabel("N values")
         plt.xlabel("Time scale")
         plt.legend(["Euler solution + Noise", "Numerical Solution"])
@@ -48,7 +48,6 @@ class ODENumerical:
 
     def least_squares_calculator(self, array):
         self.euler_solution()
-        self.euler_with_noise()
         least_squares = 0
         first_term = self.n_init
         for i, j in enumerate(self.euler_solution_array):
@@ -60,17 +59,26 @@ class ODENumerical:
         return least_squares
 
     def scoring_func(self):
+        x_values = np.arange(0, len(self.euler_solution_array), 1)
         self.minimization_sol, es = cma.fmin2(self.least_squares_calculator,[0.095, 10], 0.5)
         print(self.minimization_sol)
+        print("="*80)
         print("Updating the values of lambda and c")
         self.lambda_val = self.minimization_sol[0]
         self.c = self.minimization_sol[1]
         new_fit = self.euler_solution()
-        plt.plot(self.euler_with_noise_array)
-        plt.plot(new_fit)
+        plt.scatter(x_values, self.euler_with_noise_array, color = "yellow", edgecolors="black")
+        plt.plot(x_values, new_fit, "r")
+        plt.ylabel("N values")
+        plt.xlabel("Time scale")
+        plt.legend([ "CMA Fit", "Noisy distribution"])
+        plt.title("Plotting best fit using CMA-ES Algorithm")
         plt.show()
 
 a = ODENumerical()
+print(a.lambda_val)
+print(a.c)
+a.plot_func()
 a.scoring_func()
-
-
+print(a.lambda_val)
+print(a.c)
