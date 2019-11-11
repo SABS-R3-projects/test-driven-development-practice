7bfafcab82bb016f8620bb03830633c8c2178b4f
DavAug <david.augustin@gmx.net>
2019-11-11 12:24:48 +0000

    :white_check_marks: adding tests for inference

--- PAID/InferenceMethod/inference.py
+++ PAID/InferenceMethod/inference.py
@@ -12,10 +12,10 @@ class InferenceProblem(object):
 
         self.model = model
         self.data_time = data[0, :]
-        self.data_y = data[0, :]
+        self.data_y = data[1, :]
 
 
-    def __objective_function(self, parameters):
+    def _objective_function(self, parameters):
         # setting lambda
         model = lambda t, x: self.model(t, x, parameters[0])
         # instantiate Eulerintegrable model
@@ -27,13 +27,13 @@ class InferenceProblem(object):
         h = (t_final - t_0) / number_steps
         numerical_estimate = ODEmodel.integrate(h=h, t_0=t_0, t_final=t_final, y_0=parameters[1])
 
-        squared_distance = (self.data_y - numerical_estimate) ** 2
+        squared_distance = np.sum((self.data_y - numerical_estimate) ** 2)
 
         return squared_distance
 
 
     def infer_parameters(self, initial_parameters, step_size):
-        xopt, es = cma.fmin2(self.__objective_function, initial_parameters, step_size)
+        xopt, es = cma.fmin2(self._objective_function, initial_parameters, step_size)
         return xopt, es
 
 
--- tests/InferenceMethod/test_inference.py
+++ tests/InferenceMethod/test_inference.py
@@ -27,7 +27,7 @@ def test_infer_parameters():
 
     problem = InferenceProblem(ODEmodel, data)
 
-    initial_parameters = [3.0, 0.01]
+    initial_parameters = [3.0, 0.2]
     step_size = 0.1
     estimated_parameters, idk = problem.infer_parameters(initial_parameters, step_size)
 
@@ -37,4 +37,49 @@ def test_infer_parameters():
     assert True
 
 
+def test_objective_function():
+    """test model: static: y = 1.
+      test case 1 - null: make sure squared distance is zero for the exact model parameters.
+      test case 2 - Comparing to manufactured data points y = 2. Hence squared distance is 1 * number of data points.
+    """
+    ### exact parameters
+    exact_parameters = [np.nan, 1.0]
+    Lambda, x_0 = exact_parameters
 
+    """test case 1:"""
+    ### generate data
+    data_time = np.linspace(0, 10, 1000)
+    exact_solution = np.ones(1000)
+
+    data_y = exact_solution
+    data = np.vstack(tup=(data_time, data_y))
+
+    ODEmodel = lambda t, x, Lambda: 0
+
+    problem = InferenceProblem(ODEmodel, data)
+
+    squared_distance = problem._objective_function(exact_parameters)
+    expected_squared_distance = 0.0
+
+    if not np.isclose(a=squared_distance, b=expected_squared_distance, atol=1.0e-08):
+        print('squared_distance: ', squared_distance, expected_squared_distance)
+        raise ValueError("Objective function does not return 0 when it is supposed to.")
+
+
+    """test case 2:"""
+    ### generate data
+    data_time = np.linspace(0, 10, 1000)
+    exact_solution = np.ones(1000)
+
+    data_y = exact_solution + 1
+    data = np.vstack(tup=(data_time, data_y))
+
+    ODEmodel = lambda t, x, Lambda: 0
+
+    problem = InferenceProblem(ODEmodel, data)
+
+    squared_distance = problem._objective_function(exact_parameters)
+    expected_squared_distance = 1.0 * len(data_y)
+
+    if not np.isclose(a=squared_distance, b=expected_squared_distance, atol=1.0e-08):
+        raise ValueError("Objective function does not seem to compute the squared distance properly.")
