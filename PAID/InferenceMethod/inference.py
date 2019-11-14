aa0ac9dc46c0f085ee2edabaf4a2100bc01e2aca
DavAug <david.augustin@gmx.net>
2019-11-14 15:23:17 +0000

    :construction: MCMC intermediate stage

--- PAID/InferenceMethod/inference.py
+++ PAID/InferenceMethod/inference.py
@@ -9,7 +9,10 @@ from PAID.EulerMethod.euler import euler
 class AbstractInferenceProblem(ABC):
     # This method infers the parameters of a model that capture some observed data according to some objective function the best.
     @abstractmethod
-    def infer_parameters(self, y_0: float, initial_parameters: List[float], step_size: float) -> np.ndarray:
+    def infer_parameters(self,
+                         y_0: float,
+                         initial_parameters: List[float],
+                         step_size: float) -> np.ndarray:
         raise NotImplementedError('Method not implemented')
 
     # This method graphically visualises the infered model and the provided data.
@@ -18,7 +21,10 @@ class AbstractInferenceProblem(ABC):
         raise NotImplementedError('Method not implemented')
 
 class InferenceProblem(AbstractInferenceProblem):
-    def __init__(self, model: Callable[[float, float, List[float]], float], data: np.ndarray, h=0.001) -> None:
+    def __init__(self,
+                 model: Callable[[float, float, List[float]], float],
+                 data: np.ndarray,
+                 h=0.001) -> None:
         if data.ndim != 2:
             raise AssertionError('data must be a 2 dimensional np.array of the form [[t1, t2, ...], [y1, y2, ...]]')
         if data.shape[0] != 2:
@@ -115,19 +121,129 @@ class InferenceProblem(AbstractInferenceProblem):
 
 
 class MCMCInferenceProblem(InferenceProblem):
-    def __init__(self, model: Callable[[float, float, List[float]], float], data: np.ndarray, h=0.001) -> None:
-        super(MCMCInferenceProblem, self).__init__(self, model, h)
+    def __init__(self,
+                 model: Callable[[float, float, List[float]], float],
+                 data: np.ndarray,
+                 h=0.001) -> None:
+        super(MCMCInferenceProblem, self).__init__(self, model, data, h)
 
-    def infer_parameters(self):
-        # find posterior distributions for parameters (choice for prior, choice for noise model?, number iterations)
-        ## propose step (choice for step distribution)
-        ## compute posterior
-        ## accept/ reject step based on ratio
-        pass
+    def infer_parameters(self,
+                         initial_parameters: np.ndarray,
+                         valid_parameter_interval: np.ndarray,
+                         sampling_stepsize: np.ndarray,
+                         max_iterations=1000) -> List[np.ndarray]:
+        self.min_parameters = valid_parameter_interval[:, 0]
+        self.max_parameters = valid_parameter_interval[:, 1]
+        self.number_parameters = len(initial_parameters)
+        self.sampling_stepsize = sampling_stepsize
+        posteriors = self._find_posterior(initial_parameters, max_iterations)
+
+        optimal_parameters = np.empty(shape=self.number_parameters)
+        parameter_std = np.empty(shape=self.number_parameters)
+        for param_id, parameter_posterior in enumerate(posteriors):
+            hist, param_values = parameter_posterior
+            max_id = np.argmax(hist)
+            optimal_parameters[param_id] = param_values[max_id]
+            #TODO:
+            #parameter_std[param_id] = self._compute_std(parameter_posterior)
+
+        return [optimal_parameters, parameter_std]
 
     def plot(self):
         # plot parameter posteriors
         # plot ideal solution
         # plot distribution of solution based on sampling parameters from posterior.
         pass
+
+    def _find_posterior(self, initial_parameters: np.ndarray, max_iterations: int) -> List[np.ndarray]:
+        parameter_history = np.empty(shape=(self.number_parameters, max_iterations))
+        parameter_history[:, 0] = initial_parameters
+
+        current_parameters = initial_parameters
+        number_accepted_parameters = 0 # book-keeping
+        for _ in range(max_iterations):
+            proposed_parameters = self._propose_step(current_parameters)
+            log_posterior_ratio = self._compute_log_posterior_ratio(proposed_parameters, current_parameters)
+            is_accepted = self._are_parameters_accepted(log_posterior_ratio)
+
+            if is_accepted:
+                number_accepted_parameters += 1
+                current_parameters = proposed_parameters
+                parameter_history[:, number_accepted_parameters] = current_parameters
+
+        posteriors = self._convert_history_to_distribution(parameter_history)
+
+        return posteriors
+
+    def _propose_step(self, current_parameters: np.ndarray) -> np.ndarray:
+        covariance_matrix = np.diag(self.sampling_stepsize)
+        parameter_proposal = np.random.multivariate_normal(mean=current_parameters, cov=covariance_matrix, size=1)
+
+        return parameter_proposal
+
+    def _compute_log_posterior_ratio(self, proposed_parameters: np.ndarray, current_parameters: np.ndarray):
+        """Computes the log-posterior ratio of the current parameter set and the proposed one assuming Gaussian noise. The variance
+        of the noise is also inferred by the algorithm. We assume uniform priors for all parameters.
+        TODO: provide option for priors other than uniform.
+
+        Returns:
+            log_posterior_ratio {float} -- ratio of log-posteriors of proposed and current parameter set.
+        """
+        for id_p, parameter in enumerate(proposed_parameters):
+            if (parameter < self.min_parameters[id_p]) or (parameter > self.max_parameters):
+                # parameters out of bounds, i.e. prior weight is zero and thus leads to rejection.
+                return -np.inf
+
+        
+
+        #return posterior_ratio
+        pass
+
+    def _are_parameters_accepted(self, log_posterior_ratio: float) -> Bool:
+        # check whether proposed parameters are accepted
+        pass
+
+    def _convert_history_to_distribution(self, parameter_history: np.ndarray) -> List[np.ndarray]:
+        # convert history of samples into distributions. mean of bin egdes is taken as corresponding parameter value.
+        pass
+
+    def _objective_function(self, parameters: List[float]) -> float:
+        """Least squares objective function to be minimised in the process of parameter inference.
+
+        Arguments:
+            parameters {List[float]} -- Set of parameters that are used to solve the ODE model.
+                The last parameter i.e. parameters[-1] is assumed to be the initial value of the
+                state variable.
+        Return:
+            Point-wise squared distance between the data and the ODE model.
+        """
+        # setting lambda
+        ODEmodel = lambda t, x: self.model(t, x, parameters[0])
+        # instantiate ODE model
+        model = euler(ODEmodel)
+        t_0 = self.data_time[0]
+        t_final = self.data_time[-1]
+        numerical_estimate = model.integrate(h=self.h, t_0=t_0, t_final=t_final, y_0=parameters[-1])
+        interpolated_solution = self._interpolate_numerical_solution(numerical_estimate)
+        interpolated_solution_yvalues = interpolated_solution[1, :]
+        squared_distance = np.sum((self.data_y - interpolated_solution_yvalues) ** 2)
+
+        return squared_distance
+
+    def _interpolate_numerical_solution(self, numerical_solution: np.ndarray) -> np.ndarray:
+        """Interpolates the numerical solution of the ODE to evaluate it at the time points corresponding to the data.
+
+        Arguments:
+            numerical_solution {np.ndarray} -- 2 dimensional array found by numerically solving the IVP (times x y_values).
+
+        Returns:
+            numerical_estimate {np.ndarray} -- Returns interpolated solution matching the time points of the input data.
+        """
+        integration_time = numerical_solution[0, :]
+        integration_values = numerical_solution[1, :]
+        numerical_estimate = np.interp(x=self.data_time, xp=integration_time, fp=integration_values)
+
+        interpolated_solution = np.vstack(tup=(self.data_time, numerical_estimate))
+
+        return interpolated_solution
 
