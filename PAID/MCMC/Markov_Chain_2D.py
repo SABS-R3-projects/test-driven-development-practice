57e96b75b9c72c707f1449727e6bb098728fd0bb
Anna Carbery <anna.carbery@dtc.ox.ac.uk>
2019-11-15 11:54:14 +0000

    :art: making it nicer

--- PAID/MCMC/Markov_Chain_2D.py
+++ PAID/MCMC/Markov_Chain_2D.py
@@ -37,9 +37,37 @@ class result_dist:
         self.N_0_data = []
         self.Lambda_data = []
 
+class decision:
+    def accept_or_reject_1(self, accepted_guess, next_guess, results, guess_number):
+        if next_guess.error < accepted_guess.error:
+            accepted_guess = next_guess
+            if guess_number > 10000:
+                results.N_0_data.append(accepted_guess.N_0)
+                results.Lambda_data.append(accepted_guess.Lambda)
+            return accepted_guess
+        else:
+            accepted_guess = self.accept_or_reject_2(accepted_guess, next_guess, results, guess_number)
+        return accepted_guess
+
+    def accept_or_reject_2(self, accepted_guess, next_guess, results, guess_number):
+        ratio = accepted_guess.error/next_guess.error
+        draw = random.uniform(0, 1)
+        if draw > ratio:
+            accepted_guess = next_guess
+            if guess_number > 10000:
+                results.N_0_data.append(accepted_guess.N_0)
+                results.Lambda_data.append(accepted_guess.Lambda)
+        return accepted_guess
+
+def plot(results):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.hist(results.N_0_data, bins=50)
+    ax2.hist(results.Lambda_data, bins=50)
+    plt.show()
+
 def main():
     data = target_dist()
-
+    dec = decision()
     accepted_N_0, accepted_lambda = 0.9, 0.9 #initial guesses
     accepted_guess = guess(accepted_lambda, accepted_N_0, data.times)
     accepted_guess.error = accepted_guess.calc_error(data.data, data.times)
@@ -48,36 +76,16 @@ def main():
     prop = proposal_dist()
     guess_number = 1
 
-    while guess_number < 10000:
+    while guess_number < 50000:
         next_N_0 = accepted_guess.N_0 + random.choice(prop.x)
         if 0.0 < next_N_0 < 1.0:
             next_guess = guess(accepted_guess.Lambda + random.choice(prop.y), next_N_0, data.times)
             next_guess.error = next_guess.calc_error(data.data, data.times)
-
-            if next_guess.error < accepted_guess.error:
-                accepted_guess = next_guess
-                if guess_number > 2000:
-                    results.N_0_data.append(accepted_guess.N_0)
-                    results.Lambda_data.append(accepted_guess.Lambda)
-
-            else:
-                ratio = accepted_guess.error/next_guess.error
-                draw = random.uniform(0, 1)
-                if draw > ratio:
-                    accepted_guess = next_guess
-
-                    if guess_number > 2000:
-                        results.N_0_data.append(accepted_guess.N_0)
-                        results.Lambda_data.append(accepted_guess.Lambda)
-
+            accepted_guess = dec.accept_or_reject_1(accepted_guess, next_guess, results, guess_number)
             guess_number += 1
     return results, accepted_guess
 
 
 results, accepted_guess = main()
-print(len(results.N_0_data))
+plot(results)
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.hist(results.N_0_data, bins=45)
-ax2.hist(results.Lambda_data, bins=50)
-plt.show()
--- tests/MCMC_test.py
+++ tests/MCMC_test.py
@@ -1,14 +1,33 @@
 import unittest
 from PAID.MCMC.Markov_Chain_2D import *
 
-def test_something():
+def test_error_calc():
     data = target_dist()
     new = guess(0.15, 0.35, data.times)
-    new.curve = np.ones(len(data.times))
+    new.curve = np.zeros(len(data.times)) + 0.1
     data.data = np.zeros(len(data.times))
-    new.calc_error(data.data, data.times)
+    new.error = new.calc_error(data.data, data.times)
+    assert 4.9 < new.error < 5.0
 
-    assert True
+def test_ac_re_1():
+    data = target_dist()
+    new, newer = guess(0.15, 0.35, data.times), guess(0.15, 0.35, data.times)
+    new.error = 2
+    newer.error = 1
+    dec = decision()
+    acc = dec.accept_or_reject_1(new, newer, result_dist(), 5)
+    assert acc == newer
+
+def test_ac_re_2():
+    data = target_dist()
+    new, newer = guess(0.15, 0.35, data.times), guess(0.15, 0.35, data.times)
+    res = result_dist()
+    dec = decision()
+    for i in range(100):
+        new.error = 1
+        newer.error = 9
+        dec.accept_or_reject_1(new, newer, res, 10100)
+    assert len(res.N_0_data) > 20 and len(res.Lambda_data) > 20
 
 
 if __name__ == '__main__':
